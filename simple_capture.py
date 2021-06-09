6f795a9ee81ee048c39ccf5cb2c9f4eba2918095
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-06-10 00:49:29 +0900

    init

---
+++ simple_capture.py
@@ -0,0 +1,23 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    cv2.imshow('Capture Image', frame)
+
+    key_input = cv2.waitKey(1) & 0xFF
+
+    if key_input == ord('q'):
+        break
+
+    if key_input == ord('c'):
+        cv2.imwrite('fisheye_test.jpg', frame)
+
+cap.release()
+cv2.destroyAllWindows()
